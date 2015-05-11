7615c9672cb27f490d7cc79d2b24fb835d906a76
Patrick Schlaepfer <patrick@schlaepfer.com>
2015-05-11 23:07:46 +0200

    Works now with database access

--- src/main/java/com/schlaepfer/springservice/controller/HomeController.java
+++ src/main/java/com/schlaepfer/springservice/controller/HomeController.java
@@ -1,13 +1,13 @@
 package com.schlaepfer.springservice.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
-    
-    @RequestMapping("/home")
-    public String home() {
-        return "index";
-    }
+
+	@RequestMapping("/home")
+	public String home() {
+		return "index";
+	}
 }
--- src/main/java/com/schlaepfer/springservice/repo/BinaryDataRepository.java
+++ src/main/java/com/schlaepfer/springservice/repo/BinaryDataRepository.java
@@ -2,14 +2,14 @@ package com.schlaepfer.springservice.repo;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.schlaepfer.springservice.entity.BinaryData;
 
-@RepositoryRestResource
-public interface BinaryDataRepository extends CrudRepository<BinaryData, Integer> {
+@RepositoryRestResource(collectionResourceRel = "binarydata", path = "binarydata")
+public interface BinaryDataRepository extends PagingAndSortingRepository<BinaryData, Integer> {
 
-    List<BinaryData> findByLangId(@Param("langid") int langId);
+	List<BinaryData> findByLangId(@Param("langid") int langId);
 }
